2f9a12ce6667ee63c04fc5ced52ce430cb7c4eab
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-27 18:13:23 +0530

    Added method to find frequency of word in a sentence

--- Program.cs
+++ Program.cs
@@ -5,6 +5,23 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome tho the Hash Table Problems");
+
+            string sentence = "To be or not to be";
+            string[] words = sentence.Split(' ');
+
+            HashTable<string, int> frequency = new HashTable<string, int>(words.Length);
+
+            foreach (string word in words)
+            {
+                int count = frequency.Get(word);
+                if (count == default(int))
+                {
+                    count = 0;
+                }
+                frequency.Add(word, count + 1);
+            }
+
+            frequency.Display();
         }
 
     }
